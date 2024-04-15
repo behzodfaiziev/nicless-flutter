f5c04153cbf7cec30cced990152ab3d213d09a4b
Behzod <behzod.faiziev@gmail.com>
2024-04-15 14:17:36 +0300

    added dark mode style for maps

--- lib/core/widgets/maps/maps_widget_mixin.dart
+++ lib/core/widgets/maps/maps_widget_mixin.dart
@@ -3,4 +3,16 @@ part of 'maps_widget.dart';
 mixin MapsWidgetMixin on State<MapsWidget> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
+
+  @override
+  void dispose() {
+    _controller.future.then((controller) {
+      controller.dispose();
+    });
+    super.dispose();
+  }
+
+  Future<String> _loadMapStyle() async {
+    return rootBundle.loadString(JsonConst.instance.darkModeMapsStyle);
+  }
 }
