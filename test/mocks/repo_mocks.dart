cd8c64d2d9ebe7f2bf308eaf9d0b3a47ebae9332
Behzod <behzod.faiziev@gmail.com>
2024-04-25 17:27:58 +0300

    SaveVapeData use case test done

--- test/mocks/repo_mocks.dart
+++ test/mocks/repo_mocks.dart
@@ -2,9 +2,12 @@ import 'package:mocktail/mocktail.dart';
 import 'package:nicless_flutter/features/auth/domain/repos/auth_repo.dart';
 import 'package:nicless_flutter/features/bluetooth/domain/repo/bluetooth_repo.dart';
 import 'package:nicless_flutter/features/home/domain/repo/home_repo.dart';
+import 'package:nicless_flutter/features/onboard/domain/repo/onboarding_repo.dart';
 
-class AuthRepoMock extends Mock implements AuthRepo {}
+final class AuthRepoMock extends Mock implements AuthRepo {}
 
-class BluetoothRepoMock extends Mock implements BluetoothRepo {}
+final class BluetoothRepoMock extends Mock implements BluetoothRepo {}
 
-class HomeRepoMock extends Mock implements HomeRepo {}
+final class HomeRepoMock extends Mock implements HomeRepo {}
+
+final class OnboardingRepoMock extends Mock implements OnboardingRepo {}
