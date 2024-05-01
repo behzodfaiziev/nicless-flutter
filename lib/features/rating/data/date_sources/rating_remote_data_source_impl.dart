b6d13ca7b3b903950f0d8d1b74c0cd730b3358de
Behzod <behzod.faiziev@gmail.com>
2024-05-01 11:58:40 +0300

    updated rating data

--- lib/features/rating/data/date_sources/rating_remote_data_source_impl.dart
+++ lib/features/rating/data/date_sources/rating_remote_data_source_impl.dart
@@ -1,3 +1,5 @@
+import '../../../../core/error/exceptions/api_exception.dart';
+import '../../../../core/error/failures/api_failure.dart';
 import '../../../../product/data_objects/params/pagination_params.dart';
 import '../models/rating_model.dart';
 import 'rating_remote_data_source.dart';
@@ -7,35 +9,47 @@ class RatingRemoteDataSourceImpl implements RatingRemoteDataSource {
 
   @override
   Future<List<RatingModel>> getWeeklyRating(PaginationParams params) async {
-    await Future<void>.delayed(const Duration(seconds: 1));
-    return mockRatings;
+    try {
+      await Future<void>.delayed(const Duration(seconds: 1));
+      return mockRatings;
+    } catch (e) {
+      throw APIException(message: e.toString());
+    }
   }
 
   @override
   Future<List<RatingModel>> getMonthlyRating(PaginationParams params) async {
-    await Future<void>.delayed(const Duration(seconds: 1));
-    return mockRatings;
+    try {
+      await Future<void>.delayed(const Duration(seconds: 1));
+      return mockRatings;
+    } catch (e) {
+      throw APIException(message: e.toString());
+    }
   }
 
   @override
   Future<List<RatingModel>> getYearlyRating(PaginationParams params) async {
-    await Future<void>.delayed(const Duration(seconds: 1));
-    return mockRatings;
+    try {
+      await Future<void>.delayed(const Duration(seconds: 1));
+      return mockRatings;
+    } catch (e) {
+      throw APIException(message: e.toString());
+    }
   }
 
   final List<RatingModel> mockRatings = [
-    RatingModel(id: '1', rank: 1, averageCO2: 216, name: 'Izmir'),
-    RatingModel(id: '2', rank: 2, averageCO2: 378, name: 'Ankara'),
-    RatingModel(id: '3', rank: 3, averageCO2: 512, name: 'Istanbul'),
-    RatingModel(id: '4', rank: 4, averageCO2: 513, name: 'Antalya'),
-    RatingModel(id: '5', rank: 5, averageCO2: 528, name: 'Bursa'),
-    RatingModel(id: '6', rank: 6, averageCO2: 542, name: 'Marmaris'),
-    RatingModel(id: '7', rank: 7, averageCO2: 563, name: 'Çorlu'),
-    RatingModel(id: '8', rank: 8, averageCO2: 761, name: 'Adana'),
-    RatingModel(id: '9', rank: 9, averageCO2: 812, name: 'Kayseri'),
-    RatingModel(id: '10', rank: 10, averageCO2: 905, name: 'Mersin'),
-    RatingModel(id: '11', rank: 11, averageCO2: 924, name: 'Muratpasa'),
-    RatingModel(id: '12', rank: 12, averageCO2: 936, name: 'Denizli'),
-    RatingModel(id: '13', rank: 13, averageCO2: 945, name: 'Tekirdag'),
+    const RatingModel(id: '1', rank: 1, averageCO2: 216, name: 'Izmir'),
+    const RatingModel(id: '2', rank: 2, averageCO2: 378, name: 'Ankara'),
+    const RatingModel(id: '3', rank: 3, averageCO2: 512, name: 'Istanbul'),
+    const RatingModel(id: '4', rank: 4, averageCO2: 513, name: 'Antalya'),
+    const RatingModel(id: '5', rank: 5, averageCO2: 528, name: 'Bursa'),
+    const RatingModel(id: '6', rank: 6, averageCO2: 542, name: 'Marmaris'),
+    const RatingModel(id: '7', rank: 7, averageCO2: 563, name: 'Çorlu'),
+    const RatingModel(id: '8', rank: 8, averageCO2: 761, name: 'Adana'),
+    const RatingModel(id: '9', rank: 9, averageCO2: 812, name: 'Kayseri'),
+    const RatingModel(id: '10', rank: 10, averageCO2: 905, name: 'Mersin'),
+    const RatingModel(id: '11', rank: 11, averageCO2: 924, name: 'Muratpasa'),
+    const RatingModel(id: '12', rank: 12, averageCO2: 936, name: 'Denizli'),
+    const RatingModel(id: '13', rank: 13, averageCO2: 945, name: 'Tekirdag'),
   ];
 }
