16b05859854b38809b8173da20b66118aa8f53b2
Behzod <behzod.faiziev@gmail.com>
2024-07-18 01:15:19 +0300

    removed deprecated network manager

--- lib/core/managers/network_v1/models/response_model.dart
+++
@@ -1,8 +0,0 @@
-import '../interface/i_error_model.dart';
-import '../interface/i_fire_response_model.dart';
-import '../interface/i_network_model.dart';
-
-class ResponseModel<T, E extends INetworkModel<E>?>
-    extends IResponseModel<T?, E> {
-  ResponseModel({T? data, IErrorModel<E>? error}) : super(data, error);
-}
