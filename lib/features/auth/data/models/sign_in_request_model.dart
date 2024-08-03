08ac36214d585e3b8d8e8323a2f492cc7767c79a
Behzod <behzod.faiziev@gmail.com>
2024-08-03 22:07:19 +0300

    added sign in and sign up models

---
+++ lib/features/auth/data/models/sign_in_request_model.dart
@@ -0,0 +1,44 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:vexana/vexana.dart';
+
+part 'sign_in_request_model.g.dart';
+
+@JsonSerializable()
+class SignInRequestModel extends INetworkModel<SignInRequestModel>
+    with EquatableMixin {
+  SignInRequestModel({
+    this.email,
+    this.password,
+    this.gRecaptchaResponse,
+  });
+
+  factory SignInRequestModel.fromJson(Map<String, dynamic> json) =>
+      _$SignInRequestModelFromJson(json);
+
+  final String? email;
+  final String? password;
+  final String? gRecaptchaResponse;
+
+  @override
+  SignInRequestModel fromJson(Map<String, dynamic> json) =>
+      SignInRequestModel.fromJson(json);
+
+  @override
+  Map<String, dynamic>? toJson() => _$SignInRequestModelToJson(this);
+
+  @override
+  List<Object?> get props => [email, password, gRecaptchaResponse];
+
+  SignInRequestModel copyWith({
+    String? email,
+    String? password,
+    String? gRecaptchaResponse,
+  }) {
+    return SignInRequestModel(
+      email: email ?? this.email,
+      password: password ?? this.password,
+      gRecaptchaResponse: gRecaptchaResponse ?? this.gRecaptchaResponse,
+    );
+  }
+}
